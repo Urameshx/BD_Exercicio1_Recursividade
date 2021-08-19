fc889c5e02053102fca5dd3ee002a008e8326f19
Urameshx <88640471+Urameshx@users.noreply.github.com>
2021-08-19 17:27:29 -0300

    Update ED_Ex1_Recursividade.java

--- src/controller/ED_Ex1_Recursividade.java
+++ src/controller/ED_Ex1_Recursividade.java
@@ -7,8 +7,15 @@ public class ED_Ex1_Recursividade {
 	}
 	
 	public String troca(String string, int i) {
+		//Condição de parada = entrada com 0 string
+		/*Entrada de um caractere, retornará a própria entrada
+		*/
+		
 		if (i == 0) {
 			return "";
+		/*Entrada com mais de um caractere, virará nova string e será ordenada a partir 
+		  do último caractere*/
+			
 		} else { 
 			return string.substring(i - 1, i) + troca(string, i - 1);
 		}
